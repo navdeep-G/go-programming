8e8d7059abfeba0a9b4ac2deb6b3a39221a0cb40
navdeep-G <mr.navdeepgill@gmail.com>
2017-05-19 19:14:23 -0700

    Type conversions

---
+++ src/typeconversions/type-conversions.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+Type conversions
+The expression T(v) converts the value v to the type T.
+
+Some numeric conversions:
+
+var i int = 42
+var f float64 = float64(i)
+var u uint = uint(f)
+Or, put more simply:
+
+i := 42
+f := float64(i)
+u := uint(f)
+Unlike in C, in Go assignment between items of different type requires an
+explicit conversion.
+*/
+func main() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+}
